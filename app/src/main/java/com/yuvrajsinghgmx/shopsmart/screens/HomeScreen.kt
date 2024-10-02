0f43ede2d74a522c5d0b801d4b9d115cdc125477
Avinash <donotsmile9@gmail.com>
2024-10-02 22:08:46 +0530

    Completely changed the UI and made it look more professional.
     Multiple Items deletion Allowed
     Added New Way to Add items
     Added Dialog Box

--- app/src/main/java/com/yuvrajsinghgmx/shopsmart/screens/HomeScreen.kt
+++ app/src/main/java/com/yuvrajsinghgmx/shopsmart/screens/HomeScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
