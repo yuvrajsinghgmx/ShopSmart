1c60fd6b86c14c0e660ad2d5b119520bd66f4155
shreyansh1563 <shreyansh6153@gmail.com>
2024-11-04 01:02:50 +0530

    favorite screen UI added

--- app/src/main/java/com/yuvrajsinghgmx/shopsmart/navigation/Navigation.kt
+++ app/src/main/java/com/yuvrajsinghgmx/shopsmart/navigation/Navigation.kt
@@ -36,6 +36,7 @@ import androidx.navigation.navArgument
 import com.yuvrajsinghgmx.shopsmart.screens.appinfo.AppVersionScreen
 import com.yuvrajsinghgmx.shopsmart.screens.auth.EmailSignUpScreen
 import com.yuvrajsinghgmx.shopsmart.screens.auth.SignUpScreen
+import com.yuvrajsinghgmx.shopsmart.screens.home.FavoriteScreen
 import com.yuvrajsinghgmx.shopsmart.screens.home.HomeScreen
 import com.yuvrajsinghgmx.shopsmart.screens.home.ListScreen
 import com.yuvrajsinghgmx.shopsmart.screens.home.ShopSmartNavBar
@@ -175,10 +176,6 @@ fun Navigation(viewModel: ShoppingListViewModel, navController: NavHostControlle
 
             composable("List") {
                 ListScreen(viewModel = viewModel, navController = navController)
-            }
-
-            composable("Favorites") {
-                ComingSoonScreen("Favorites",navController)
             }
 
             composable("Profile") {
@@ -430,6 +427,10 @@ fun Navigation(viewModel: ShoppingListViewModel, navController: NavHostControlle
             })){
                 val index = it.arguments?.getInt("itemsIndex")?:1
                 ProductDetails(index = index, navController = navController)
+            }
+
+            composable("Favorites"){
+                FavoriteScreen()
             }
         }
     }
