92ebd9dcf65009efb2bbb37729553bdf231f3ade
Yuvraj Singh <qomfortzone@gmail.com>
2025-07-23 08:41:01 +0530

    ShopSmart 2.0

---
+++ android-new/app/src/main/java/com/yuvrajsinghgmx/shopsmart/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.yuvrajsinghgmx.shopsmart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.tooling.preview.Preview
+import com.yuvrajsinghgmx.shopsmart.ui.theme.ShopSmartTheme
+import org.intellij.lang.annotations.JdkConstants
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShopSmartTheme {
+                Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                    "This is ShopSmart"
+                }
+            }
+        }
+    }
+}
+
