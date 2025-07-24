26465c66b3fc3c38184edb148e7eeb2b451e87c3
deesha <deeshajaya428@gmail.com>
2025-07-24 21:15:24 +0530

    Product Details Screen

---
+++ android-new/app/src/main/java/com/yuvrajsinghgmx/shopsmart/modelclass/Product.kt
@@ -0,0 +1,16 @@
+package com.yuvrajsinghgmx.shopsmart.modelclass
+
+class Product(
+    val productId:Int ,
+    val name: String,
+    val price: String,
+    val category: String,
+    val review : String,
+    val description: String,
+    val imageUrl: List<String>,
+    val shopName: String,
+    val shopNumber:String,
+    val distance: String
+) {
+
+}
