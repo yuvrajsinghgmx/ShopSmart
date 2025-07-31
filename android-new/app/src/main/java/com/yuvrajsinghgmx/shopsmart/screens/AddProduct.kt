49f2881711a4048d33a1062a4f4ea71393867be1
Yuvraj Singh <yuvrajsinghgmx@gmail.com>
2025-07-31 17:29:15 +0530

    Fix

--- android-new/app/src/main/java/com/yuvrajsinghgmx/shopsmart/screens/AddProduct.kt
+++ android-new/app/src/main/java/com/yuvrajsinghgmx/shopsmart/screens/AddProduct.kt
@@ -1,18 +1,13 @@
 package com.yuvrajsinghgmx.shopsmart.screens
 
 import android.net.Uri
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -23,18 +18,14 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
-import com.example.addproductscreen.ui.theme.AddProductScreenTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
