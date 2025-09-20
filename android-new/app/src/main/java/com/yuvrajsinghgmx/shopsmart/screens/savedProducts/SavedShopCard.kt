f3bdaac954c957d0e36d36289d11e52b779c1bab
deesha <deeshajaya428@gmail.com>
2025-09-20 09:42:38 +0530

    FIX: Saved Screen

--- android-new/app/src/main/java/com/yuvrajsinghgmx/shopsmart/screens/savedProducts/SavedShopCard.kt
+++ android-new/app/src/main/java/com/yuvrajsinghgmx/shopsmart/screens/savedProducts/SavedShopCard.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -60,7 +61,12 @@ fun SavedShopCard(
                 error = painterResource(R.drawable.error),
                 modifier = Modifier
                     .height(120.dp)
-                    .fillMaxWidth()
+                    .fillMaxWidth(),
+                contentScale = if(shop.shopImages.isEmpty()){
+                    ContentScale.Fit
+                }else{
+                    ContentScale.Crop
+                }
             )
             IconButton(
                 onClick = { /* needs to be implemented */ },
